67b65218615388d63f823c7e668ec7d09faf6901
Rumpelstilzli <david_wild@gmx.ch>
2024-09-03 10:27:09 +0200

    station two weekname to japanese

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -1,4 +1,14 @@
 from datetime import datetime
+ # Mapping of English weekday names to Japanese
+english_to_japanese = {
+    'Monday': '月曜日',
+    'Tuesday': '火曜日',
+    'Wednesday': '水曜日',
+    'Thursday': '木曜日',
+    'Friday': '金曜日',
+    'Saturday': '土曜日',
+    'Sunday': '日曜日'
+}
 
 def solution_station_2(date_str):
     # Parse the input string to a date object
@@ -7,4 +17,4 @@ def solution_station_2(date_str):
     # Get the weekday name
     weekday_name = date_obj.strftime('%A')
     
-    return weekday_name
+    return english_to_japanese[weekday_name]
